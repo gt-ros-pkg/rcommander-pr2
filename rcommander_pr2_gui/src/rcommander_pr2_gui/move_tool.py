29adc178982a4b8a0ee80ff80d71fa16915454b7
Hai Nguyen <haidai@gmail.com>
2011-11-14 17:18:58 -0500

    Uncommitted change.  Oops!

--- rcommander_pr2_gui/src/rcommander_pr2_gui/move_tool.py
+++ rcommander_pr2_gui/src/rcommander_pr2_gui/move_tool.py
@@ -2,7 +2,7 @@
 import rospy
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
-import rcommander_core.tool_utils as tu
+import rcommander.tool_utils as tu
 import numpy as np
 import actionlib_msgs.msg as am
 import smach
