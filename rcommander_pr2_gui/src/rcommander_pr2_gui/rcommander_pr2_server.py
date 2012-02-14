bb82eafa87c06680015cd4390f6affea9625950a
Hai Nguyen <haidai@gmail.com>
2012-02-14 16:11:48 -0500

    moving server to pr2 specific branch as rviz frontend is pr2 specific

--- rcommander_pr2_gui/src/rcommander_pr2_gui/rcommander_pr2_server.py
+++ rcommander_pr2_gui/src/rcommander_pr2_gui/rcommander_pr2_server.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 import roslib; roslib.load_manifest('rcommander_pr2_gui')
-
+import pr2_utils as pu
+import rcommander.rcommander_auto_server as rcs
+import sys
 import tf 
 import rospy
 
-import pr2_utils as pu
-import sys
-import rcommander.rcommander_server as rcs
-
-rospy.init_node('rcommander_actions_server')
+# "home/haidai/Desktop/rcommander")
+rospy.init_node('rcommander_pr2_server')
+path = sys.argv[1]
 tf = tf.TransformListener()
 pr2 = pu.PR2(tf)
-rcs.run(pr2)
+rcs.run(pr2, path)
