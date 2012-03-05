eef09fba2ca89dcb02238417c545360ba572835d
Hai Nguyen <haidai@gmail.com>
2012-03-05 16:11:22 -0500

    removed autogenerated files from repository.

--- ptp_arm_action/src/ptp_arm_action/msg/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._LinearMovementFeedback import *
-from ._LinearMovementActionResult import *
-from ._LinearMovementActionGoal import *
-from ._LinearMovementResult import *
-from ._LinearMovementActionFeedback import *
-from ._LinearMovementAction import *
-from ._LinearMovementGoal import *
