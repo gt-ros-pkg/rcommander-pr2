c97f4c20f5ff7dd8ef42a36b39cfa6ba0b3a4cde
Hai Nguyen <haidai@gmail.com>
2012-05-03 14:22:22 -0400

    updated rcommander_pr2 launch files to match new api in rcommander_core

--- rcommander_pr2_gui/src/rcommander_pr2_gui/rcommander_pr2.py
+++ rcommander_pr2_gui/src/rcommander_pr2_gui/rcommander_pr2.py
@@ -8,4 +8,4 @@ import tf
 rospy.init_node('rcommander', anonymous=True)
 tf = tf.TransformListener()
 pr2 = pu.PR2(tf)
-rc.run_rcommander(pr2, tf, ['all', 'pr2'])
+rc.run_rcommander(['default', 'default_frame', 'pr2'], pr2, tf)
