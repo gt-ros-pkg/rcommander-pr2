7aa4a543f96cc4c078a0833300a64738db709898
Hai Nguyen <haidai@gmail.com>
2012-05-14 14:03:35 -0400

    added angles from pr2_tuck_arms rcommander_tuckarm_node.py

--- rcommander_pr2_gui/scripts/rcommander_tuckarm_node.py
+++ rcommander_pr2_gui/scripts/rcommander_tuckarm_node.py
@@ -8,58 +8,35 @@ import rcommander_pr2_gui.msg as rm
 import pr2_common_action_msgs.msg as ca 
 import actionlib_msgs.msg as am
 
+l_arm_tucked = [0.06024, 1.248526, 1.789070, -1.683386, -1.7343417, -0.0962141, -0.0864407]
+r_arm_tucked = [-0.023593, 1.1072800, -1.5566882, -2.124408, -1.4175, -1.8417, 0.21436]
+l_arm_untucked = [ 0.4,  1.0,   0.0,  -2.05,  0.0,  -0.1,  0.0]
+r_arm_untucked = [-0.4,  1.0,   0.0,  -2.05,  0.0,  -0.1,  0.0]
+r_arm_approach = [0.039, 1.1072, 0.0, -2.067, -1.231, -1.998, 0.369]
+r_arm_up_traj = [[-0.4,  0.0,   0.0,  -2.05,  0.0,  -0.1,  0.0]]
+
+
 class RCommanderTuckArmNode:
     def __init__(self):
         #Setup an action server where we call the real tuck arm
         self.tuckarm_as = actionlib.SimpleActionServer('rcommander_tuckarms', rm.RCTuckArmsAction, 
 					                    execute_cb=self.action_cb, auto_start=False)
-        self.tuckarm_as.start()
-        self.tuckarm_client = actionlib.SimpleActionClient('tuck_arms', ca.TuckArmsAction)
         self.controller_manager = ControllerManager()
+
+        #self.tuckarm_as.start()
+        #self.tuckarm_client = actionlib.SimpleActionClient('tuck_arms', ca.TuckArmsAction)
 
 
     def action_cb(self, request):
-        #Make sure we're using the joint controllers
-        rospy.loginfo('starting joint mode')
-        arm = 'both'
-        #self.controller_manager.joint_mode(arm)
-        
-        rospy.loginfo('sent goal')
-        goal = ca.TuckArmsGoal()
-        goal.tuck_left = request.tuck_left
-        goal.tuck_right = request.tuck_right
-        self.tuckarm_client.send_goal(goal)
+        request.tuck_left
 
-        r = rospy.Rate(15)
-        state = None
-        while not rospy.is_shutdown():
-            if self.tuckarm_as.is_preempt_requested():
-                rospy.loginfo('goal canceled')
-                self.tuckarm_client.cancel_goal()
-                self.tuckarm_as.set_preempted()
-                return
 
-            state = self.tuckarm_client.get_state()
-            if (state not in [am.GoalStatus.ACTIVE, am.GoalStatus.PENDING]):
-                rospy.loginfo('finished goal')
-                break 
-            r.sleep()
+        status, started, stopped = self.controller_manager.joint_mode(self.arm)
+        self.arm_obj.set_poses(np.column_stack(wps), np.cumsum(np.array(times)), block=False)
+        client = self.arm_obj.client
+        state = client.get_state()
 
-        rospy.loginfo('Waiting for result from tuck arm...')
-        self.tuckarm_client.wait_for_result()
 
-        rospy.loginfo('Got result!')
-        tuck_result = self.tuckarm_client.get_result()
-        rcresult = rm.RCTuckArmsResult()
-        rcresult.tuck_left = tuck_result.tuck_left
-        rcresult.tuck_right = tuck_result.tuck_right
-
-        if state == am.GoalStatus.SUCCEEDED:
-            self.tuckarm_as.set_succeeded(rcresult)
-        elif state == am.GoalStatus.PREEMPTED:
-            self.tuckarm_as.set_preempted()
-        else:
-            self.tuckarm_as.set_aborted(rcresult)
 
 if __name__ == '__main__':
     rospy.init_node('RCTuckArms')
@@ -71,3 +48,44 @@ if __name__ == '__main__':
 
 
 
+        ##Make sure we're using the joint controllers
+        #rospy.loginfo('starting joint mode')
+        #arm = 'both'
+        ##self.controller_manager.joint_mode(arm)
+        #
+        #rospy.loginfo('sent goal')
+        #goal = ca.TuckArmsGoal()
+        #goal.tuck_left = request.tuck_left
+        #goal.tuck_right = request.tuck_right
+        #self.tuckarm_client.send_goal(goal)
+
+        #r = rospy.Rate(15)
+        #state = None
+        #while not rospy.is_shutdown():
+        #    if self.tuckarm_as.is_preempt_requested():
+        #        rospy.loginfo('goal canceled')
+        #        self.tuckarm_client.cancel_goal()
+        #        self.tuckarm_as.set_preempted()
+        #        return
+
+        #    state = self.tuckarm_client.get_state()
+        #    if (state not in [am.GoalStatus.ACTIVE, am.GoalStatus.PENDING]):
+        #        rospy.loginfo('finished goal')
+        #        break 
+        #    r.sleep()
+
+        #rospy.loginfo('Waiting for result from tuck arm...')
+        #self.tuckarm_client.wait_for_result()
+
+        #rospy.loginfo('Got result!')
+        #tuck_result = self.tuckarm_client.get_result()
+        #rcresult = rm.RCTuckArmsResult()
+        #rcresult.tuck_left = tuck_result.tuck_left
+        #rcresult.tuck_right = tuck_result.tuck_right
+
+        #if state == am.GoalStatus.SUCCEEDED:
+        #    self.tuckarm_as.set_succeeded(rcresult)
+        #elif state == am.GoalStatus.PREEMPTED:
+        #    self.tuckarm_as.set_preempted()
+        #else:
+        #    self.tuckarm_as.set_aborted(rcresult)
