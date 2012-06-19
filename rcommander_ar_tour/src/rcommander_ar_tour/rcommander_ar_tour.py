9949ca369acbef4ec9c4121f0f112d27f0fc8208
Hai Nguyen <haidai@gmail.com>
2012-06-18 23:32:19 -0400

    listens to ar pose topic to determine visiblity

--- rcommander_ar_tour/src/rcommander_ar_tour/rcommander_ar_tour.py
+++ rcommander_ar_tour/src/rcommander_ar_tour/rcommander_ar_tour.py
@@ -1,13 +1,17 @@
-import roslib; roslib.load_manifest("interactive_markers")
+import roslib; roslib.load_manifest("rcommander_ar_tour")
 import rospy
-import interactive_markers.interactive_marker_server as ims
 import std_msgs.msg as stdm
+import interactive_markers.interactive_marker_server as ims
 import interactive_markers.menu_handler as mh
+import ar_pose.msg as ar_msg
+import rcommander_ar_pose.utils as rap
+import rcommander_pr2_gui.tf_utils as tfu
+import tf
+
 import re
 import copy
 
 DEFAULT_LOC = [[0.,0.,0.], [0.,0.,0.,1.]]
-
 
 def interactive_marker(name, pose, scale):
     int_marker = ims.InteractiveMarker()
@@ -24,7 +28,6 @@ def interactive_marker(name, pose, scale):
     int_marker.name = name
     int_marker.description = name
     return int_marker
-
 
 def make_rviz_marker(scale):
     marker = ims.Marker()
@@ -87,6 +90,7 @@ def pose_to_tup(p):
     return [p.position.x, p.position.y, p.position.z], \
             [p.orientation.x, p.orientation.y, p.orientation.z, p.orientation.w]
 
+
 class TagDatabase:
 
     def __init__(self):
@@ -113,10 +117,13 @@ class TagDatabase:
     def tag_ids(self):
         return self.database.keys()
 
+    def has_id(self, tagid):
+        return tagid in self.database.keys()
+
 
 class MarkerDisplay:
     
-    def __init__(self, tagid, server, tag_database):
+    def __init__(self, tagid, server, tag_database, tf_listener):
         self.tagid = tagid
 
         self.server = server
@@ -125,7 +132,7 @@ class MarkerDisplay:
         #self.make_target_marker('target_' + self.tagid, self.tag_database.get(self.tagid)['target_location'], .5)
         self.make_ar_marker('ar_' + self.tagid, self.tag_database.get(self.tagid)['ar_location'], .5)
         self.has_point = False
-
+        self.tf_listener = tf_listener
         #self.behavior_menu = BehaviorMenu('menu_' + self.tag_id, self.server)
 
     def make_ar_marker(self, name, pose, scale):
@@ -145,15 +152,16 @@ class MarkerDisplay:
         int_marker.controls.append(copy.deepcopy(menu_control))
 
         menu_handler = mh.MenuHandler()
-        menu_handler.insert( "First Entry", callback=self.process_feedback )
-        menu_handler.insert( "Second Entry", callback=self.process_feedback )
-        sub_menu_handle = menu_handler.insert( "Submenu" )
-        menu_handler.insert( "First Entry", parent=sub_menu_handle, callback=self.process_feedback )
-        menu_handler.insert( "Second Entry", parent=sub_menu_handle, callback=self.process_feedback )
+        menu_handler.insert("First Entry", callback=self.process_feedback)
+        menu_handler.insert("Second Entry", callback=self.process_feedback)
+        sub_menu_handle = menu_handler.insert("Submenu")
+        menu_handler.insert("First Entry", parent=sub_menu_handle, callback=self.process_feedback)
+        menu_handler.insert("Second Entry", parent=sub_menu_handle, callback=self.process_feedback)
         menu_handler.apply(server, int_marker.name)
 
     def make_target_marker(self, name, pose, scale):
         int_marker = interactive_marker(name, pose, scale)
+        int_marker.header.frame_id = 'map' #self.tagid
         int_marker.description = ''
         int_marker.controls.append(make_sphere_control(name, scale/2.))
         int_marker.controls[0].markers[0].color = stdm.ColorRGBA(1,0,0,.5)
@@ -167,7 +175,11 @@ class MarkerDisplay:
         if self.has_point:
             self.server.erase(self.target_marker.name)
         else:
-            self.make_target_marker('target_' + self.tagid, self.tag_database.get(self.tagid)['target_location'], .5)
+            p_ar = self.tag_database.get(self.tagid)['target_location']
+            m_ar = tfu.tf_as_matrix(p_ar)
+            map_T_ar = tfu.transform('map', self.tagid, self.tf_listener, t=0)
+            p_map = tfu.matrix_as_tf(map_T_ar * m_ar)
+            self.make_target_marker('target_' + self.tagid, p_map, .5)
             
         self.server.applyChanges()
         self.has_point = not self.has_point
@@ -183,30 +195,92 @@ class MarkerDisplay:
         if target_match != None and feedback.event_type == ims.InteractiveMarkerFeedback.POSE_UPDATE:
             self.tag_database.update_target_location(self.tagid, pose_to_tup(feedback.pose))
 
-
-
 class ARTour:
 
     def __init__(self):
         self.SERVER_NAME = 'ar_tour'
-        self.tag_database = TagDatabase()
-        print 'initiating tag database'
         rospy.init_node(self.SERVER_NAME)
+
+        self.broadcaster = tf.TransformBroadcaster()
+        self.tf_listener = tf.TransformListener()
+
+        self.tag_database = TagDatabase()
         self.server = ims.InteractiveMarkerServer(self.SERVER_NAME)
 
         self.markers = {}
         for tagid in self.tag_database.tag_ids():
-            display = MarkerDisplay(tagid, self.server, self.tag_database)
-            self.markers[tagid] = display
-            #[self.server.insert(m, display.process_feedback) for m in display.get_markers()]
-
-        #self.bmenu = BehaviorMenu(self.server)
+            self.make_marker(tagid)
         self.server.applyChanges()
+        self.ar_pose_sub = rospy.Subscriber("ar_pose_marker", ar_msg.ARMarkers, self.ar_marker_cb)
+        self.marker_visibility = {}
+        print 'Ready!'
+
+    def make_marker(self, tagid):
+        if not self.markers.has_key(tagid):
+            display = MarkerDisplay(tagid, self.server, self.tag_database, self.tf_listener)
+            self.markers[tagid] = display
+        else:
+            print 'Marker', tagid, 'exists! Ignoring make_marker call.'
+
+    def ar_marker_cb(self, msg):
+        #print 'called back'
+        self.marker_visibility = {}
+        for marker in msg.markers:
+            self.marker_visibility['4x4_' + str(marker.id)] = True
+        #print 'cb', len(msg.markers)
+
+    def _keep_frames_updated(self):
+        print '<<<<<<<<<<<<<<<'
+        #ar_frames = rap.find_ar_frames(self.tf_listener)
+        visible = self.marker_visibility.keys()
+        print 'visible', visible
+        
+        #Publish frames that are not visible
+        for tagid in self.tag_database.tag_ids():
+            #frameid = '/' + tagid
+            if not (tagid in visible):
+                position, orientation = self.tag_database.get(tagid)['ar_location']
+                self.broadcaster.sendTransform(position, orientation, rospy.Time.now(), tagid, '/map')
+                print tagid, ' is not visible. creating transforms.'
+        
+        #make sure all visible frames are in database
+        markers_changed = False
+        for tagid in visible:
+            try:
+                ar_location = self.tf_listener.lookupTransform('map', tagid, rospy.Time(0))
+                if not self.tag_database.has_id(tagid):
+                    print 'inserted', tagid, 'into database'
+                    self.tag_database.insert(tagid, ar_location)
+                    self.make_marker(tagid)
+                    markers_changed = True
+                else:
+                    self.tag_database.get(tagid)['ar_location'] = ar_location
+            except tf.ExtrapolationException, e:
+                print 'exception', e
+            except tf.ConnectivityException, e:
+                print 'exception', e
+            except tf.LookupException, e:
+                print 'exception', e.__class__
+
+        if markers_changed:
+            self.server.applyChanges()
+        print '>>>>>>>>>>>>>>>'
+
+
+    def _save_database(self):
+        pass
 
     def run(self):
-        print 'ready.'
+        r = rospy.Rate(2)
+        i = 0
+        #print 'Ready.'
+        while not rospy.is_shutdown():
+            self._keep_frames_updated()
+            r.sleep()
+            if (i % (5*5) == 0):
+                self._save_database()
+            i += 1
         rospy.spin()
-
 
 
 if __name__=="__main__":
@@ -227,7 +301,10 @@ if __name__=="__main__":
 #   On sensing create a 
 #       
 #   A menu marker class monitors the state of a directory
- 
+
+
+#
+#   A 
  
  
  
