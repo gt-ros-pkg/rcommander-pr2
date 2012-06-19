cef92bef5fcf3560ec28d753cbfcbe0601c235b0
Hai Nguyen <haidai@gmail.com>
2012-06-19 02:59:05 -0400

    fixed missing imports in utils.py

--- rcommander_ar_pose/src/rcommander_ar_pose/utils.py
+++ rcommander_ar_pose/src/rcommander_ar_pose/utils.py
@@ -1,8 +1,9 @@
 #import tf
 #self.broadcaster = tf.TransformBroadcaster()
 #self.tf_listener = tf.TransformListener()
+import re
 
-def find_ar_frames(tf_listener)
+def find_ar_frames(tf_listener):
     all_frames = tf_listener.getFrameStrings()
     ar_transforms = []
     fourfour = re.compile('^/4x4_\d+')
