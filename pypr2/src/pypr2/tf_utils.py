3e6625cf773b40c8e0a32331e4e462b0116e3a11
Hai Nguyen <haidai@gmail.com>
2012-08-07 10:08:10 -0700

    fixed typo

--- pypr2/src/pypr2/tf_utils.py
+++ pypr2/src/pypr2/tf_utils.py
@@ -5,9 +5,9 @@ import rospy
 def tf_as_matrix(tup):
     return np.matrix(tr.translation_matrix(tup[0])) * np.matrix(tr.quaternion_matrix(tup[1])) 
 
-def transform(to_frame, from_frame, tflistener, t=0):
+def transform(to_frame, from_frame, tf_listener, t=0):
     tf_listener.waitForTransform(to_frame, from_frame, rospy.Time(t), rospy.Duration(10))
-    return tf_as_matrix(tflistener.lookupTransform(to_frame, from_frame, rospy.Time(t)))
+    return tf_as_matrix(tf_listener.lookupTransform(to_frame, from_frame, rospy.Time(t)))
 
 def tf_as_matrix(tup):
     return np.matrix(tr.translation_matrix(tup[0])) * np.matrix(tr.quaternion_matrix(tup[1])) 
