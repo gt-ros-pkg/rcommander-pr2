ce58c52f7ab346fbe5a36accf3eb1be6e3c443e9
Hai Nguyen <delver@Siegfried2.local>
2012-11-15 18:00:35 -0800

    more comments...

--- pycontroller_manager/src/pycontroller_manager/pycontroller_manager.py
+++ pycontroller_manager/src/pycontroller_manager/pycontroller_manager.py
@@ -7,19 +7,29 @@ LOADED_CTRLS_PARAMS = {
     'l' : "/controller_manager/loaded_ctrls/left_arm",
 }
 
+## Manages which controller should be running (joint or cartesian)
 class ControllerManager:
 
+    ## Constructor
     def __init__(self):
         # LoadController        
-        self.load = rospy.ServiceProxy('pr2_controller_manager/load_controller', pmm.LoadController)
+        self.load = rospy.ServiceProxy(\
+                'pr2_controller_manager/load_controller', 
+                pmm.LoadController)
 
         # UnloadController        
-        self.unload = rospy.ServiceProxy('pr2_controller_manager/unload_controller', pmm.UnloadController)
+        self.unload = rospy.ServiceProxy(\
+                'pr2_controller_manager/unload_controller', 
+                pmm.UnloadController)
 
         # SwitchController
-        self._switch_controller = rospy.ServiceProxy('pr2_controller_manager/switch_controller', pmm.SwitchController)
+        self._switch_controller = rospy.ServiceProxy(\
+                'pr2_controller_manager/switch_controller', 
+                pmm.SwitchController)
 
-        self.list_controllers = rospy.ServiceProxy('pr2_controller_manager/list_controllers', pmm.ListControllers)
+        self.list_controllers = rospy.ServiceProxy(\
+                'pr2_controller_manager/list_controllers', 
+                pmm.ListControllers)
 
         self.joint_controllers = {}
         self.cart_controllers = {}
@@ -27,17 +37,18 @@ class ControllerManager:
             self.joint_controllers[arm] = arm + '_arm_controller'
             self.cart_controllers[arm] = cart_controller_name = arm + '_cart'
 
-    ##
-    # Given the list of starting controllers, look for the possible arm controllers which
+    ## Given the list of starting controllers, look for the possible
+    # arm controllers which
     # conflict with those which are currently running.
-    # KelseyH
+    # @param start_con list of controllers that will be started
     def get_possible_running_ctrls(self, start_con):
         #rospy.loginfo("get_possible... in")
         # find the sides the start controllers are on
         arms_starting = []
         for con in start_con:
             for arm in ['l', 'r']:
-                if con == self.joint_controllers[arm] or con == self.cart_controllers[arm]:
+                if con == self.joint_controllers[arm] \
+                        or con == self.cart_controllers[arm]:
                     arms_starting.append(arm)
 
         # find the possible controllers running corresponding to those sides
@@ -45,7 +56,8 @@ class ControllerManager:
         for arm in ['l', 'r']:
             if arm in arms_starting:
                 try:
-                    possible_ctrls.extend(rospy.get_param(LOADED_CTRLS_PARAMS[arm]))
+                    possible_ctrls.extend(rospy.get_param(\
+                            LOADED_CTRLS_PARAMS[arm]))
                 except KeyError, e:
                     pass
 
@@ -53,16 +65,20 @@ class ControllerManager:
         stop_con = []
         con_states = self.list_controllers()
         for i, controller in enumerate(con_states.controllers):
-            if controller in possible_ctrls and con_states.state[i] == 'running':
+            if controller in possible_ctrls \
+                    and con_states.state[i] == 'running':
                 stop_con.append(controller)
         stop_con_no_start = []
         for con in stop_con:
             if not con in start_con:
                 stop_con_no_start.append(con)
-        #rospy.loginfo("get_possible... out")
-        #rospy.loginfo(str(arms_starting) + str(start_con) + str(possible_ctrls) + str(stop_con_no_start))
         return stop_con_no_start
 
+    ## Switches between two sets of controllers
+    # @param start_con list of arm controllers to start.
+    # @param stop_con list of arm controllers to stop.
+    # @return response from switch controller service, list of
+    #       controllers started, list of controllers stopped
     def switch(self, start_con, stop_con):
         stop_con.extend(self.get_possible_running_ctrls(start_con)) # KelseyH
 
@@ -73,66 +89,70 @@ class ControllerManager:
         #Add the ones that are loaded but not running.
         for idx, controller in enumerate(con.controllers):
             if controller in start_con:
-                #print 'found', controller, 'it\'s state is', con.state[idx], len(con.state[idx]), con.state[idx].__class__
                 if con.state[idx] != 'running':
-                    #print 'ControllerManager: adding controller', controller
                     valid_start.append(controller)
-                #else:
-                #    print 'ControllerManager: not adding', controller
 
             if controller in stop_con:
                 if con.state[idx] != 'stopped':
                     valid_stop.append(controller)
 
-            #print controller, con.state[idx]
-
         #Add all controllers not loaded! But load them first
         for n in start_con:
             if not n in con.controllers:
-                #print 'loading controller', n
                 self.load(n)
                 valid_start.append(n)
 
         if len(start_con) > 0:
-            #print 'ControllerManager: starting', valid_start, 'stopping', valid_stop
-            resp = self._switch_controller(valid_start, valid_stop, pmm.SwitchControllerRequest.STRICT)
+            resp = self._switch_controller(valid_start, valid_stop, 
+                    pmm.SwitchControllerRequest.STRICT)
             return resp.ok, valid_start, valid_stop
         else:
-            #print 'ControllerManager: not starting or stopping any controllsers'
             return None, valid_start, valid_stop
 
+    ## Puts the given arm in joint mode
+    ## @param arm either 'left', 'right or 'both'
     def joint_mode(self, arm):
         #get current state
         statuses = True
         all_started = []
         all_stopped = []
         if arm == 'left' or arm == 'both':
-            status, started, stopped = self.switch([self.joint_controllers['l']], [self.cart_controllers['l']])
+            status, started, stopped = self.switch(
+                    [self.joint_controllers['l']], 
+                    [self.cart_controllers['l']])
             statuses = status and statuses
             all_started += started
             all_stopped += stopped
 
         if arm == 'right' or arm == 'both':
-            status, started, stopped = self.switch([self.joint_controllers['r']], [self.cart_controllers['r']])
+            status, started, stopped = self.switch(
+                    [self.joint_controllers['r']], 
+                    [self.cart_controllers['r']])
             statuses = status and statuses
             all_started += started
             all_stopped += stopped
 
         return statuses, all_started, all_stopped
 
+    ## Puts the given arm in Cartesian mode
+    ## @param arm either 'left', 'right or 'both'
     def cart_mode(self, arm):
         statuses = True
         all_started = []
         all_stopped = []
         if arm == 'left' or arm == 'both':
             #print 'switchleft'
-            status, started, stopped = self.switch([self.cart_controllers['l']], [self.joint_controllers['l']])
+            status, started, stopped = self.switch(
+                    [self.cart_controllers['l']], 
+                    [self.joint_controllers['l']])
             statuses = status and statuses
             all_started += started
             all_stopped += stopped
         if arm == 'right' or arm == 'both':
             #print 'switchright'
-            status, started, stopped = self.switch([self.cart_controllers['r']], [self.joint_controllers['r']])
+            status, started, stopped = self.switch(
+                    [self.cart_controllers['r']], 
+                    [self.joint_controllers['r']])
             statuses = status and statuses
             all_started += started
             all_stopped += stopped
